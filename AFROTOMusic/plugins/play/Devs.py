12ce7beaf7a2ed6e85aa4a5a83febdb6ae84b178
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 00:46:32 +0200

    Update Devs.py

--- AFROTOMusic/plugins/play/Devs.py
+++ AFROTOMusic/plugins/play/Devs.py
@@ -1,96 +1,37 @@
 import asyncio
+
 import os
 import time
 import requests
+from config import START_IMG_URL
 from pyrogram import filters
 import random
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
+from AFROTOMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AFROTOMusic import app
 from random import  choice, randint
 
-def get_file_id(msg: Message):
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            # "contact",
-            # "dice",
-            # "poll",
-            # "location",
-            # "venue",
-            "sticker",
-        ):
-            obj = getattr(msg, message_type)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
-
-@app.on_message(
-    command(["المطور","البوب","امير","مطور السورس","مبرمج السورس"])
-    & filters.group
-  
-)
-async def yas(client, message):
-    usr = await client.get_chat("VIP_ALPOP")
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
-                ],
-            ]
-        ),
-    )
-    
-@app.on_message(
-    command(["المطور مارو","مارو","المبرمج"])
-    & filters.group
-  
-)
-async def yas(client, message):
-    usr = await client.get_chat("j_s_9")
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
-                ],
-            ]
-        ),
-    )
-    
-@app.on_message(
-    command(["مطورين","مطورين السورس","المطورين","سورس","السورس"])
-  
+#          
+                
+@app.on_message(filters.command(["عفرتو","مطور السورس","مبرمج السورس","المطور"],"")
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/f698f60484b7aef0d6f29.jpg",
-        caption=f"""↯︙اهلا بك عزيزي {message.from_user.mention}\n↯︙مطورين سورس البوب ميوزك""",
+        photo=f"https://telegra.ph/file/96857cb597b588139fdd5.jpg",
+        caption=f"""◉ 𝙽𝙰𝙼𝙴 : ❪[ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪](https://t.me/IIUll_l)❫
+◉ 𝚄𝚂𝙴𝚁 : ❪ @IIUll_l ❫
+◉ 𝙸𝙳      : ❪ `5904216848` ❫
+◉ 𝙱𝙸𝙾    : ❪ for me (@UI_VM)  ❫""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "‹ : 𝗗𝗘𝗩 𝗔𝗟𝗣𝗢𝗣 : ›", url=f"https://t.me/VIP_ALPOP"), 
+                        "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪", url=f"https://t.me/IIUll_l"), 
                  ],[
-                    InlineKeyboardButton(
-                        "‹ : 𝗗𝗘𝗩 𝗠𝗔𝗥𝗢² : ›", url=f"https://t.me/j_s_9"),
-                ],[
-                    InlineKeyboardButton(
-                        "‹ : 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗟𝗣𝗢𝗣 : ›", url=f"https://t.me/SOURCE_ALPOP"),
+                   InlineKeyboardButton(
+                        "「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」", url=f"https://t.me/UI_VM"),
                 ],
 
             ]
@@ -98,25 +39,32 @@ async def huhh(client: Client, message: Message):
         ),
 
     )
-
-@app.on_message(command(["تخ"]) & filters.group)
-async def huhh(client, message):
-    to_id = int(ahmed.split("to")[-1].split("in")[0])
-    from_id = int(ahmed.split("ahmed")[-1].split("to")[0])
-    in_id = int(caption.split("in")[-1])
-    to_url = f"tg://openmessage?user_id={to_id}"
-    from_url = f"tg://openmessage?user_id={from_id}"
-    ahmed = message.text
-    await message.reply_animation(
-        animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
-        caption=f"""↯︙قتل ↫ ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\nالضحيه دا 😢 ↫ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
-    )
-    reply_markup=InlineKeyboardMarkup(
-
-       [
-           [
-               InlineKeyboardButton(
-                   "‹ : 𝖬𝖺𝖳𝗋𝗂x 𝖳𝖾𝖠𝗆 : ›", url=f"https://t.me/XMATTMX"),
-           ],
-       ]
-    ),
+@app.on_message(
+    command(["مطور", "المطور"])
+    & filters.group
+  
+)
+async def kimmyy(client: Client, message: Message):
+    usr = await client.get_users(OWNER)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(OWNER, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""**⩹━★⊷⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝐕𝐄𝐆𝐀⌯⊶★━⩺**
+                    
+🔥 ¦𝚆𝙾𝙽𝙴𝚁 :[{usr.first_name}](https://t.me/{OWNER})
+📀 ¦𝚄𝚂𝙴𝚁 :@{OWNER} 
+🆔 ¦𝙸𝙳 :`{usr.id}`
+ 
+**⩹━★⊷⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝐕𝐄𝐆𝐀⌯⊶★━⩺** """, 
+reply_markup=InlineKeyboardMarkup(
+          [               
+            [            
+              InlineKeyboardButton (name, url=f"https://t.me/{OWNER}")
+            ],                   
+          ]              
+       )                 
+    )                    
+                    sender_id = message.from_user.id
+                    sender_name = message.from_user.first_name
+                    await app.send_message(OWNER, f"الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name}")
+                    return await app.send_message(config.LOG_GROUP_ID, f"الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name}")
+      
