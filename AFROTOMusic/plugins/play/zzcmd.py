4aa2b2c817fbcb952f668acd32d940e4283a143a
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 17:00:15 +0200

    Update zzcmd.py

--- AFROTOMusic/plugins/play/zzcmd.py
+++ AFROTOMusic/plugins/play/zzcmd.py
@@ -28,7 +28,7 @@ async def zzzback(_, query: CallbackQuery):
                         "‹ اوامر المطور ›", callback_data="zzzdv"),
                 ],[
                     InlineKeyboardButton(
-                        "‹ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗟𝗣𝗢𝗣 ›", url="https://t.me/SOURCE_ALPOP"),
+                        "ᯓ 𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾 𝅘𝅥𝅯", url="https://t.me/UI_VM"),
                 ],
             ]
         ),
