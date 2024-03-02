52ebce9c8502e79a975bbc020bdb7a4d4d8e1384
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 13:48:28 +0200

    Update start.py

--- AFROTOMusic/utils/inline/start.py
+++ AFROTOMusic/utils/inline/start.py
@@ -6,40 +6,35 @@ from AFROTOMusic import app
 
 def start_panel(_):
     buttons = [
-    [
+        [
+            InlineKeyboardButton(
+                text="⭓ADD✘ME",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        
          [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
         [
             InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
             InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=Commands&admin=ban_users+restrict_members+delete_messages+add_admins+change_info+invite_users+pin_messages+manage_call+manage_chat+manage_video_chats+promote_members",
-            )
-        ],
     ]
     return buttons
-
-
 
 
 def private_panel(_):
     buttons = [
-       
-InlineKeyboardButton(
-            [
-           [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
+        [
+            InlineKeyboardButton(
+                text="⭓ADD✘ME",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        
+         [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
         [
             InlineKeyboardButton(text="ᗪEᐯEᒪOᑭEᖇ", user_id=config.OWNER_ID),
             InlineKeyboardButton(text="ᑕᕼᗩᑎᑎEᒪ", url=config.SUPPORT_CHANNEL),
-                [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=Commands&admin=ban_users+restrict_members+delete_messages+add_admins+change_info+invite_users+pin_messages+manage_call+manage_chat+manage_video_chats+promote_members",
-            )
         ],
     ]
     return buttons
-
-
