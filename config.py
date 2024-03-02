868ffbcfa4c8a6b75f8550cb17f9e9fc68f2d75f
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 17:11:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -94,7 +94,7 @@ command = ["/"]
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/04598a67494ae0482408d.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg"
