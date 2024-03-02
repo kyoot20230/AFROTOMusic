743ae9fb401fcda34489d0890629405ce8d03521
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 15:12:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -44,6 +44,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UI_VM")
+OWNER_CHANNEL = int(getenv("OWNER_CHANNEL", None))
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
