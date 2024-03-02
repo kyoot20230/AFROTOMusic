c1e8d7436259a685f3931ddc71e715077f779f05
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-02 15:18:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -28,6 +28,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5904216848))
 
+OWNER_CHANNEL = int(getenv("OWNER_CHANNEL", None))
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
