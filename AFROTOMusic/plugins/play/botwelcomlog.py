d2b43898617860f9a66b7499dfe6bebe0afe133f
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-04 01:15:36 +0200

    Update botwelcomlog.py

--- AFROTOMusic/plugins/play/botwelcomlog.py
+++ AFROTOMusic/plugins/play/botwelcomlog.py
@@ -6,9 +6,9 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from AnonX import app
-from AnonX.utils.database import get_served_chats
-from config import LOG_GROUP_ID
+from AFROTOMusic import app
+from AFROTOMusic.utils.database import get_served_chats
+from config import LOGGER_ID
 
 
 async def lul_message(chat_id: int, message: str):
