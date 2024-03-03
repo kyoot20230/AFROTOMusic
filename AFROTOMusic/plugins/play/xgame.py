02642a1f60fc3f027f41016593a1d92fb2554cc7
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-03 03:20:31 +0200

    Update xgame.py

--- AFROTOMusic/plugins/play/xgame.py
+++ AFROTOMusic/plugins/play/xgame.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
+from strings import get_string
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 from typing import Union
