c05467617d460cb5d1e9c5a43c0ca4ec8347e455
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-05 01:25:20 +0200

    Update thumbnails.py

--- AFROTOMusic/utils/thumbnails.py
+++ AFROTOMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import FAILED
+from config import YOUTUBE_IMG_URL
 from AFROTOMusic import BOT_ID, LOGGER, app
 
 
@@ -318,4 +318,4 @@ async def gen_qthumb(videoid, user_id):
         return f"cache/que{videoid}_{user_id}.png"
     except Exception as e:
         LOGGER.error(e)
-        return FAILED
+        return YOUTUBE_IMG_URL
