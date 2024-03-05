e9ff98a5ed6c858659ec9db43c21bfd48bb8013a
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-05 03:54:46 +0200

    Update thumbnails.py

--- AFROTOMusic/utils/thumbnails.py
+++ AFROTOMusic/utils/thumbnails.py
@@ -124,14 +124,14 @@ async def get_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text(
-                (10, 10), "Arnop Music", fill="white", font=font1, align="left"
+                (10, 10), "AFROTOO MUSIC", fill="white", font=font1, align="left"
             )
             image4.text(
                 (670, 150),
                 "AFROTOO Playing",
                 fill="white",
                 font=font2,
-                stroke_width=2,
+                stroke_width=3,
                 stroke_fill="white",
                 align="left",
             )
