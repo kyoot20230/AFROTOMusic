1bddb9c9a0062457f6ce9824abcdfda8d6393a85
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-03-05 03:51:36 +0200

    Update thumbnails.py

--- AFROTOMusic/utils/thumbnails.py
+++ AFROTOMusic/utils/thumbnails.py
@@ -128,7 +128,7 @@ async def get_thumb(videoid):
             )
             image4.text(
                 (670, 150),
-                "AFYONA Playing",
+                "AFROTOO Playing",
                 fill="white",
                 font=font2,
                 stroke_width=2,
