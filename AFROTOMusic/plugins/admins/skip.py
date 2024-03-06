87138cc8746398d6913259fd05f1c408c1c74964
abdalaleK <155861080+abdalaleK@users.noreply.github.com>
2024-03-06 19:15:18 +0300

    Update skip.py

--- AFROTOMusic/plugins/admins/skip.py
+++ AFROTOMusic/plugins/admins/skip.py
@@ -15,7 +15,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    command(["تخطي", "/skip", "/next", "التالي"]) & ~BANNED_USERS
+    command(["تخطي", "/skip", "/next", "تخطي"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
