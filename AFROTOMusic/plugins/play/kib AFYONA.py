6395861b83b0d172830a8e7b5f562fcc8c681754
abdalaleK <155861080+abdalaleK@users.noreply.github.com>
2024-03-11 05:04:13 +0300

    Update kib AFYONA.py

--- AFROTOMusic/plugins/play/kib AFYONA.py
+++ AFROTOMusic/plugins/play/kib AFYONA.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from strings import get_string
 from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
 
 
 
