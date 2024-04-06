63cd0bf1d7d0eb6a97d19d8a51f9ee9329747d2a
abdalaleK <155861080+abdalaleK@users.noreply.github.com>
2024-04-06 20:08:11 +0300

    Update *السورس.py

--- AFROTOMusic/plugins/play/*السورس.py
+++ AFROTOMusic/plugins/play/*السورس.py
@@ -18,7 +18,7 @@ from random import  choice, randint
 )
 async def huhh(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://telegra.ph/file/ad25b873df9aface84c0f.mp4",
+        video=f"https://graph.org/file/aa04832e375d5e429e93b.mp4",
         caption=f"""
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼""",
         reply_markup=InlineKeyboardMarkup(
